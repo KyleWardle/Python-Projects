ed9556cb657c59ead013175013364d7e5d856261
KyleWardle <kylewardle@bspokeit.co.uk>
2019-02-08 09:36:56 +0000

    Stacks with different data types

--- stacks.py
+++ stacks.py
@@ -1,10 +1,10 @@
 arr = []
 
 arr.append(5)
-arr.append(4)
-arr.append(3)
+arr.append("4")
+arr.append("three")
 arr.append(2)
-arr.append(1)
+arr.append("one")
 
 for i in range(0, len(arr)):
     print(arr.pop())
