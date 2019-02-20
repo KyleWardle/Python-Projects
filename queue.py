3a9d9a45496d563e4e5b76bd45fa4b55e2c83f3d
Kyle Wardle <kylewardle@bspokeit.co.uk>
2019-02-20 09:49:45 +0000

    Further Refactoring

--- queue.py
+++ queue.py
@@ -1,8 +1,9 @@
 class Node:
     def __init__(self, value, next_node, previous_node):
-      self.value = value
-      self.next_node = next_node
-      self.previous_node = previous_node
+        self.value = value
+        self.next_node = next_node
+        self.previous_node = previous_node
+
 
 class Queue:
     def __init__(self):
@@ -10,37 +11,38 @@ class Queue:
         self.bottom = None
         self.length = 0
 
-    def Push(self, value):
+    def push(self, value):
         new_node = Node(value, None, None)
-        if (self.head):
+        if self.head:
             new_node.next_node = self.head
             self.head.previous_node = new_node
-        if (self.bottom == None):
+        if self.bottom is None:
             self.bottom = new_node
         self.head = new_node
-        self.length+= 1
+        self.length += 1
         return None
 
-    def Pop(self):
-        if (self.bottom):
+    def pop(self):
+        if self.bottom:
             tmp = self.bottom
-            if (tmp.previous_node):
+            if tmp.previous_node:
                 self.bottom = tmp.previous_node
             else:
                 self.bottom = None
-            self.length-= 1
+            self.length -= 1
             return tmp.value
         else:
             raise ValueError('Queue has nothing to Pop!')
 
-    def Length(self):
+    def length(self):
         return self.length
 
-    def __len__ (self):
+    def __len__(self):
         return self.length
 
     def Peek(self):
         return self.head.value
+
 
 class PriorityQueue:
     def __init__(self):
@@ -48,47 +50,48 @@ class PriorityQueue:
         self.med_priority = Queue()
         self.high_priority = Queue()
 
-    def Push(self, value, priority = 3):
-        if (priority == 3):
-            self.low_priority.Push(value)
-        if (priority == 2):
-            self.med_priority.Push(value)
-        if (priority == 1):
-            self.high_priority.Push(value)
+    def push(self, value, priority=3):
+        if priority == 3:
+            self.low_priority.push(value)
+        if priority == 2:
+            self.med_priority.push(value)
+        if priority == 1:
+            self.high_priority.push(value)
 
-    def Pop(self, highest_priority = 1):
-        if ((len(self.high_priority) > 0) and (highest_priority <= 1)):
-            return self.high_priority.Pop()
-        if ((len(self.med_priority) > 0) and (highest_priority <= 2)):
-            return self.med_priority.Pop()
-        if ((len(self.low_priority) > 0) and (highest_priority <= 3)):
-            return self.low_priority.Pop()
+    def pop(self, highest_priority=1):
+        if (len(self.high_priority) > 0) and (highest_priority <= 1):
+            return self.high_priority.pop()
+        if (len(self.med_priority) > 0) and (highest_priority <= 2):
+            return self.med_priority.pop()
+        if (len(self.low_priority) > 0) and (highest_priority <= 3):
+            return self.low_priority.pop()
 
-    def __len__ (self):
+    def __len__(self):
         return len(self.low_priority) + len(self.med_priority) + len(self.high_priority)
 
-def testQueue():
+
+def test_queue():
     queue = Queue()
-    queue.Push(3)
-    queue.Push(5)
-    queue.Push(7)
+    queue.push(3)
+    queue.push(5)
+    queue.push(7)
 
-    print("Length is : " + str(queue.Length()))
+    print("Length is : " + str(queue.length()))
+
+    print(queue.pop())
+    print(queue.pop())
+    print(queue.pop())
 
 
-    print(queue.Pop())
-    print(queue.Pop())
-    print(queue.Pop())
-
-def testPriorityQueue():
+def test_priority_queue():
     pqueue = PriorityQueue()
-    pqueue.Push(3)
-    pqueue.Push(2, 2)
-    pqueue.Push(1, 1)
+    pqueue.push(3)
+    pqueue.push(2, 2)
+    pqueue.push(1, 1)
 
-    print(pqueue.Pop(2))
-    print(pqueue.Pop())
-    print(pqueue.Pop())
+    print(pqueue.pop(2))
+    print(pqueue.pop())
+    print(pqueue.pop())
 
 
-testPriorityQueue()
+test_priority_queue()
